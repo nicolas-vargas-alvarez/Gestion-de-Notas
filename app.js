0fdbec6beb7b0ee39002998816e8022c77e2ef41
CamiloQZ <camiloqzsena@gmail.com>
2026-04-14 08:34:53 -0500

    Subo nuevo integrante

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const aprendices = [
   { nombre: "Luis",  nota: 2.8, programa: "ADSO" },
   { nombre: "Marta", nota: 3.7, programa: "Diseño Web" },
   { nombre: "Pedro", nota: 1.9, programa: "ADSO" },
-  { nombre: "Sofia", nota: 5.0, programa: "Diseño Web" }
+  { nombre: "Sofia", nota: 5.0, programa: "Diseño Web" },
+  {nombre: "Laura", nota: 2.9, programa: "Diseño Web"},
 ];
 
 // ── FUNCIÓN AUXILIAR: imprimir lista en pantalla ──
